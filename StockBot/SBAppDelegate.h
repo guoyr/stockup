a08cbb36c97dde050f182ac515285867a857f622
Robert Guo <robertguo@me.com>
2014-02-26 11:59:39 -0500

    Initial Commit

---
+++ StockBot/SBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SBAppDelegate.h
+//  StockBot
+//
+//  Created by Robert Guo on 2/26/14.
+//  Copyright (c) 2014 Robert Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StockBot/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StockBot
+//
+//  Created by Robert Guo on 2/26/14.
+//  Copyright (c) 2014 Robert Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SBAppDelegate class]));
+    }
+}
